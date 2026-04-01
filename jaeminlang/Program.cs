d87dce2668af64b4b03aa4905fbc7a8438a9018d
Pro203S <prosirea2501@gmail.com>
2026-04-02 00:27:19 +0900

    파일 존재 여부 확인

--- jaeminlang/Program.cs
+++ jaeminlang/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Stream stderr = Console.OpenStandardError();
+            stderr.Write(Encoding.UTF8.GetBytes("파일명을 제공해주세요!"));
+
+            Environment.Exit(1);
+        }
+
+        string JmlFilePath = args[0];
+        if (!File.Exists(JmlFilePath))
+        {
+            Stream stderr = Console.OpenStandardError();
+            stderr.Write(Encoding.UTF8.GetBytes("파일이 존재하지 않습니다!"));
+
+            Environment.Exit(1);
+        }
+    }
+}
