c0bccfedb37d54a79040777683aa54aabe3e28df
Pro203S <prosirea2501@gmail.com>
2026-05-08 10:07:43 +0900

    old solution 파일 삭제

--- jaeminlang/Program.cs
+++ jaeminlang/Program.cs
@@ -8,10 +8,37 @@ namespace jaeminlang
         {
             if (args.Length == 0)
             {
-                Stream stderr = Console.OpenStandardError();
-                stderr.Write(Encoding.UTF8.GetBytes("인수에 파일명을 줘야지;;"));
+                Console.WriteLine("jaeminlang\n아래에 재민랭을 입력하세요.\n");
+                for (; ; )
+                {
+                    Stream stderr = Console.OpenStandardError();
+                    try
+                    {
+                        string? line = Console.ReadLine();
+                        if (line == null || string.IsNullOrEmpty(line))
+                        {
+                            Console.WriteLine();
+                            continue;
+                        }
 
-                Environment.Exit(1);
+                        if (line.StartsWith("러스트"))
+                        {
+                            Console.WriteLine("여기서 러스트는 못쓰긴해");
+                        }
+
+                        JMLCommand cmd = new(line, new Action<int>((_) => { }));
+
+                        cmd.action();
+                        Console.WriteLine();
+                    }
+                    catch (Exception e)
+                    {
+                        stderr.Write(Encoding.UTF8.GetBytes("아니;; 재민랭 똑바로 못써??\r\n"));
+                        stderr.Write(Encoding.UTF8.GetBytes($"\r\n"));
+                        stderr.Write(Encoding.UTF8.GetBytes(e.Message + "\r\n"));
+                        stderr.Write(Encoding.UTF8.GetBytes(e.StackTrace + "\r\n"));
+                    }
+                }
             }
 
             string JmlFilePath = args[0];
